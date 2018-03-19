2cfdfa56ee5b16d0e6434f67268f4fd494bd9ac1
ice <452825089@qq.com>
2018-03-19 14:18:53 +0800

    add config

---
+++ app/src/main/java/com/meiyou/icekotlin/ImageListActivity.kt
@@ -0,0 +1,16 @@
+package com.meiyou.icekotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by mu on 18/3/19.
+ */
+class ImageListActivity:AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_list);
+
+    }
+}
